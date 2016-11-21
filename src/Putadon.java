04ece23879eda4473498a7381f9cb60c193e1ffe
Marco Climent <cem201409@gmail.com>
2016-11-21 15:57:20 +0100

    finalizado

---
+++ src/Putadon.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/*
+ * CSI Florida
+ * Reto Putadon
+ * 
+ * El siguiente código busca una letra dentro de una frase
+ * y devuelve dónde se ha encontrado y ctas veces
+ * 
+ * Hay dos errores
+ * 
+ */
+public class Putadon {
+	
+	public static void main(String[] args) {
+		//Variables inicialización
+		String frase="";
+		int numPos=0;
+		int numRepeticiones=0;
+		char letra=0;
+		
+		//Pedimos datos
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Introduce tu frase");
+		frase = sc.nextLine();
+		
+		System.out.println("Introduce la letra a buscar");
+		letra = sc.nextLine().charAt(0); //charAt(0) devuelve la primera letra
+		
+		//Buscamos las letras
+		for(int i=0;i<frase.length();i++){
+			if(letra==frase.charAt(i)){
+				numRepeticiones++;
+				System.out.println("Letra "+letra+" encontrada en la posicion "+encuentraLetra(i,numPos));
+			}
+		}
+		System.out.println("Total repeticiones "+cuentaRepeticiones(numRepeticiones));
+	
+	}
+	public static int encuentraLetra(int i, int numPos){
+		numPos=i+1;
+		return numPos;
+	}
+	public static int cuentaRepeticiones(int numRepeticiones){
+		return numRepeticiones++;
+	}
+}
